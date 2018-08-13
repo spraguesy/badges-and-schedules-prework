d70bcee570cb2e8bf0b1ea8bc9c01fd4b33d223f
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 13:14:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,17 @@
 # Write your code here.
-def bedge_maker (name)
+def badge_maker (name)
   puts "Hello, my name is #{name}"
 end
 
 def batch_badge_creator (arr)
   arr.each do |name|
-    bedge_maker(name)
+    badge_maker(name)
+  end
+end
+
+def assign_rooms (arr)
+  assigned_rooms = []
+  arr.each do |room, i|
+    assigned_rooms.push("Hello, #{room}! You'll be assigned to room #{i}!")
   end
 end
