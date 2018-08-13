f720b1091319b9bbe1bbff51d5ed81b6365ecacd
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 13:17:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,15 +4,17 @@ def badge_maker (name)
 end
 
 def batch_badge_creator (arr)
+  batch_badges = []
   arr.each do |name|
-    puts badge_maker(name)
+    batch_badges.push(badge_maker(name))
   end
+  return batch_badges
 end
 
 def assign_rooms (arr)
   assigned_rooms = []
   arr.each do |room, i|
-    puts assigned_rooms.push("Hello, #{room}! You'll be assigned to room #{i}!")
+    assigned_rooms.push("Hello, #{room}! You'll be assigned to room #{i}!")
   end
   return assigned_rooms
 end
